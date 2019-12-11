72e3addb81cc10366a375c1b8610f52fe5cb3a26
Pranit Wakhare <pranit25wakhare@gmail.com>
2019-12-11 20:34:03 +0530

    i am adding

---
+++ src/main/java/AndroidApp/Test.java
@@ -0,0 +1,11 @@
+package AndroidApp;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("created");
+		System.out.println("done ");
+		
+	}
+	
+}
---
+++ src/main/java/AndroidBrowser/Browser.java
@@ -0,0 +1,20 @@
+package AndroidBrowser;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Browser {
+
+	
+	@BeforeSuite
+	public void browse() throws IOException, InterruptedException {
+		
+		Runtime.getRuntime().exec("cmd /c start C:\\Users\\Pranit\\startAppium.bat");
+		Thread.sleep(59000L);
+		System.out.println("Hello");
+		
+	}
+	
+}
